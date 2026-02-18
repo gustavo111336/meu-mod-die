3a2f431502c0dd10169082d858b90f951557bf96
NAME <mail@gmail.com>
2026-02-18 02:28:04 -0300

    Fix: Alerta simplificado para Android32

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,21 @@
+
 #include <Geode/Geode.hpp>
 #include <Geode/modify/PlayLayer.hpp>
 
 using namespace geode::prelude;
 
-class $modify(MyDeathMod, PlayLayer) {
+class $modify(PlayLayer) {
     void destroyPlayer(PlayerObject* p0, GameObject* p1) {
-        // Chama a função original primeiro
+        // Chama a função original
         PlayLayer::destroyPlayer(p0, p1);
 
-        // Verifica se o objeto que matou o player não é nulo 
-        // para evitar que o jogo feche sozinho (crash)
-        FLAlertLayer::create(
-            "MORREU!", 
+        // Alerta simplificado para evitar erro de FMT
+        auto alert = FLAlertLayer::create(
+            "Morte", 
             "DIEE DIEE", 
             "OK"
-        )->show();
+        );
+        alert->m_noElasticity = true; // Ajuda a evitar crash em algumas versões
+        alert->show();
     }
 };
