7943e596ac23cf077314e38a8044d532c23a0901
NAME <mail@gmail.com>
2026-02-18 22:56:43 -0300

    fix: corrigindo sintaxe das chaves no main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,17 +6,26 @@ using namespace geode::prelude;
 class $modify(MyMenuLayer, MenuLayer) {
     bool init() {
         if (!MenuLayer::init()) return false;
-        auto m = this->getChildByID("right-side-menu");
-        if (m) {
-            auto s = CircleButtonSprite::createWithSpriteFrameName("geode.loader/geode-logo-outline.png");
-            auto b = CCMenuItemSpriteExtra::create(s, this, menu_selector(MyMenuLayer::onM));
-            m->addChild(b);
-            m->updateLayout();
+
+        auto menu = this->getChildByID("right-side-menu");
+        
+        if (menu) {
+            auto sprite = CircleButtonSprite::createWithSpriteFrameName("geode.loader/geode-logo-outline.png");
+            auto btn = CCMenuItemSpriteExtra::create(
+                sprite,
+                this,
+                menu_selector(MyMenuLayer::onMyButton)
+            );
+
+            btn->setID("my-custom-button");
+            menu->addChild(btn);
+            menu->updateLayout();
         }
+
         return true;
     }
-    void onM(CCObject*) {
-        FLAlertLayer::create("G", "M", "OK")->show();
+
+    void onMyButton(CCObject*) {
+        FLAlertLayer::create("GUSTAVO", "Mod Funcional!", "OK")->show();
     }
 };
-l
