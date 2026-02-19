2ffaa0c3eac5917cee1e0cb20ba3fc552f05340e
NAME <mail@gmail.com>
2026-02-18 23:30:09 -0300

    fix: removendo processamento de string para build 28

--- src/main.cpp
+++ src/main.cpp
@@ -4,51 +4,43 @@
 
 using namespace geode::prelude;
 
-// Variável global simples
+// Variável de controle
 bool g_noclipEnabled = false;
 
-// --- Hook para Noclip ---
+// --- Hook do Noclip ---
 class $modify(PlayerObject) {
     void destroyPlayer(bool p0, bool p1) {
-        if (g_noclipEnabled) {
-            return; 
-        }
+        if (g_noclipEnabled) return; 
         PlayerObject::destroyPlayer(p0, p1);
     }
 };
 
-// --- Interface no Menu ---
+// --- Hook do Menu ---
 class $modify(MyMenuLayer, MenuLayer) {
     bool init() {
         if (!MenuLayer::init()) return false;
 
         auto menu = this->getChildByID("right-side-menu");
-        
         if (menu) {
             auto btnSprite = CircleButtonSprite::createWithSpriteFrameName("geode.loader/geode-logo-outline.png");
             auto menuBtn = CCMenuItemSpriteExtra::create(
-                btnSprite,
-                this,
-                menu_selector(MyMenuLayer::onModMenu)
+                btnSprite, this, menu_selector(MyMenuLayer::onModMenu)
             );
-
-            menuBtn->setID("my-mod-toggle-btn");
+            menuBtn->setID("noclip-btn");
             menu->addChild(menuBtn);
             menu->updateLayout();
         }
-
         return true;
     }
 
     void onModMenu(CCObject*) {
         g_noclipEnabled = !g_noclipEnabled;
         
-        // Usamos const char* para evitar que o Geode tente usar o fmt::format internamente
-        const char* mensagem = g_noclipEnabled ? "Noclip: ATIVADO" : "Noclip: DESATIVADO";
-
+        // Em vez de criar uma mensagem com o status, vamos usar um título fixo
+        // Isso evita que o compilador tente converter strings
         FLAlertLayer::create(
-            "GUSTAVO MENU",
-            mensagem, 
+            "MOD STATUS", 
+            "O Noclip foi alternado!", 
             "OK"
         )->show();
     }
